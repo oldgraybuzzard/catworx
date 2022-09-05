c5c005659b4fddae06b7cf9c3928f882c2fec593
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-09-05 16:05:15 -0400

    using the Employee class

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    static List<string> GetEmployees()
+    static List<Employee> GetEmployees()
     {
-      List<string> employees = new List<string>();
+      List<Employee> employees = new List<Employee>();
       while(true)
       {
         Console.WriteLine("Please enter a name: (leave empty to exit): ");
@@ -16,22 +17,25 @@ namespace CatWorx.BadgeMaker
         {
           break;
         }
-        employees.Add(input);
+        // Create a new Employee instance
+        Employee currentEmployee = new Employee(input, "Smith");
+        // add currentEmployee
+        employees.Add(currentEmployee);
       }
       return employees;
     }
 
-    static void PrintEmployees(List<string> employees)
+    static void PrintEmployees(List<Employee> employees)
     {
       for (int i = 0; i < employees.Count; i++)
       {
-        Console.WriteLine(employees[i]);
+        Console.WriteLine(employees[i].GetFullName());
       }
     }
     static void Main(string[] args)
     {
       //This is our employee-getting code
-      List<string> employees = GetEmployees();
+      List<Employee> employees = GetEmployees();
       PrintEmployees(employees);
     }
   }
