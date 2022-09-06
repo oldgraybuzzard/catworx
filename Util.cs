32fe0be0fdaa97213d9799ed2f8fb318c3d1532d
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-09-06 16:40:21 -0400

    able to print info to badge

--- Util.cs
+++ Util.cs
@@ -47,6 +47,12 @@ namespace CatWorx.BadgeMaker
       int PHOTO_RIGHT_X = 486;
       int PHOTO_BOTTOM_Y = 517;
 
+      int COMPANY_NAME_Y = 150;
+
+      int EMPLOYEE_NAME_Y = 600;
+
+      int EMPLOYEE_ID_Y = 730;
+
       using(HttpClient client = new HttpClient())
       {
         for (int i = 0; i < employees.Count; i++)
@@ -62,10 +68,30 @@ namespace CatWorx.BadgeMaker
 
           // SKData data = background.Encode();
           // data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+          SKPaint paint = new SKPaint();
+          paint.TextSize = 42.0f;
+          paint.IsAntialias = true;
+          paint.Color = SKColors.White;
+          paint.IsStroke = false;
+          paint.TextAlign = SKTextAlign.Center;
+          paint.Typeface = SKTypeface.FromFamilyName("Arial");
+          //print company name
+          canvas.DrawText(employees[i].GetCompanyName(), BADGE_WIDTH / 2f, COMPANY_NAME_Y, paint);
+
+          paint.Color = SKColors.Black;
+          //print employee name
+          canvas.DrawText(employees[i].GetFullName(), BADGE_WIDTH / 2f, EMPLOYEE_NAME_Y, paint);
+
+          paint.Typeface = SKTypeface.FromFamilyName("Courier New");
+          //print employee id
+          canvas.DrawText(employees[i].GetId().ToString(), BADGE_WIDTH /2f, EMPLOYEE_ID_Y, paint);
+
 
           SKImage finalImage = SKImage.FromBitmap(badge);
           SKData data = finalImage.Encode();
           data.SaveTo(File.OpenWrite("data/employeeBadge.png"));
+
+          employees[i].GetCompanyName();
         }
       }
     }
